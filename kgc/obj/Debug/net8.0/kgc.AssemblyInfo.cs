d5040a0f77e9a15e809844b8fae0d81abfc03825
kiruthigarcad01 <kiruthigarcad01@gmail.com>
2024-05-15 16:19:21 +0530

    added 2 tables

--- kgc/obj/Debug/net8.0/kgc.AssemblyInfo.cs
+++ kgc/obj/Debug/net8.0/kgc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("kgc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebd720a975f65b277d31402256720e0841ec0341")]
 [assembly: System.Reflection.AssemblyProductAttribute("kgc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("kgc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
