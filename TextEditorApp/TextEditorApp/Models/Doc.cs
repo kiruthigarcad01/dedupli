a5f32c3fb83c37d58e4856906ac1ee57caa7324b
kiruthigarcad01 <kiruthigarcad01@gmail.com>
2024-06-01 17:20:18 +0530

    new pro text editir added

---
+++ TextEditorApp/TextEditorApp/Models/Doc.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TextEditorApp.Models
+{
+    public class Doc
+    {
+       
+        public int Id { get; set; }
+        public string Tittle { get; set; }
+        public string Content { get; set; }
+
+        [Required]
+        public string? UserId { get; set; }
+        
+        [ForeignKey("UserId")]
+        public IdentityUser? User { get; set; }
+
+
+    }
+}
