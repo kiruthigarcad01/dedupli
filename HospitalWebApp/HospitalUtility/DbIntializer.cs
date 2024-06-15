324a7515832b306a16d6fa86750b60555c4ea010
kiruthigarcad01 <kiruthigarcad01@gmail.com>
2024-06-15 17:28:56 +0530

    added hosp

---
+++ HospitalWebApp/HospitalUtility/DbIntializer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalUtility
+{
+    public class DbIntializer : IDbIntializer
+    {
+        private UserManager<IdentityUser> _userManager;
+        private RoleManger<IdentityRole> roleManger;
+        private ApplicationDbContext _context;
+
+        public void IDbIntialize
+        {
+            throw new NotImplementedException();
+    }
+
+
+
+}
+}
---
+++ HospitalWebApp/HospitalUtility/IDbintializer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalUtility
+{
+    public interface IDbIntializer
+    {
+       void IDbIntialize();
+    }
+}
