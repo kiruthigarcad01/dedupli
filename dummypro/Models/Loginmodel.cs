ab4a50592647aff132e577120c8496b4e46b0932
kiruthigarcad01 <kiruthigarcad01@gmail.com>
2024-07-25 15:56:02 +0530

    added inter

---
+++ dummypro/Models/Loginmodel.cs
@@ -0,0 +1,9 @@
+namespace dummypro.Models
+{
+    public class Loginmodel
+    {
+        public int? id {get; set;}
+        public string? name {get; set;}
+        public string? city {get; set;}
+    }
+}
